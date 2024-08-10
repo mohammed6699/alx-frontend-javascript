e53d3471dd5ca8fcb82917cb2395684920568c7b
root <mohammedahmed.refaat@gmail.com>
2024-08-09 17:50:39 -0700

    ./8

---
+++ 0x00-ES6_basic/8-getBudgetCurrentYear.js
@@ -0,0 +1,13 @@
+function getCurrentYear() {
+  return 2021;
+}
+
+export default function getBudgetForCurrentYear(income, gdp, capita) {
+  const budget = {};
+
+  budget[`income-${getCurrentYear()}`] = income;
+  budget[`gdp-${getCurrentYear()}`] = gdp;
+  budget[`capita-${getCurrentYear()}`] = capita;
+
+  return budget;
+}
