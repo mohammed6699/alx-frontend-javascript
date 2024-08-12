1af0bf9c4342a463c33897c9b13f414bfe24a023
root <mohammedahmed.refaat@gmail.com>
2024-08-12 13:35:39 -0700

    modify tak no.1 for requirmnets

--- 0x03-ES6_data_manipulation/0-get_list_students.js
+++ 0x03-ES6_data_manipulation/0-get_list_students.js
@@ -1,18 +1,18 @@
-export default function getListStudents() {
-	const n1 = {
-		id: 1,
-		firstName: 'Guillaume',
-		location: 'San Francisco',
+export default function getListStudents(){
+const n1 = {
+	id:1,
+	firstName:'Guillaume',
+	location:'San Francisco',
 	};
-	const n2 = {
-		id: 2,
-		firstName: 'James',
-		location: 'Columbia',
+const n2 = {
+	id:2,
+	firstName:'James',
+	location:'Columbia',
 	};
-	const n3 = {
-		id: 5,
-		firstName: 'Serena',
-		location: 'San Francisco',
-	}
-	return [n1, n2, n3];
+const n3 = {
+	id: 5,
+	firstName:'Serena',
+	location:'San Francisco',
+	};
+return [n1, n2, n3];
 }
