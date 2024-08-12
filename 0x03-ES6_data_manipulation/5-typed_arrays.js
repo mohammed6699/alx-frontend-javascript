56914f56f8ce6e902e5cf28102e619aef4913db2
root <mohammedahmed.refaat@gmail.com>
2024-08-12 13:46:21 -0700

    .task no.5 after modifing the error by adding Throw error and Uint8

--- 0x03-ES6_data_manipulation/5-typed_arrays.js
+++ 0x03-ES6_data_manipulation/5-typed_arrays.js
@@ -1,8 +1,8 @@
 export default function createInt8TypedArray (length, position, value) {
 	if (position >= length) {
-		return ('Position outside range');
+		throw new Error ('Position outside range');
 	}
 	const Buffer = new DataView (new ArrayBuffer(length), 0, length);
-	Buffer.setUnit8(position, value);
+	Buffer.setUint8(position, value);
 	return Buffer;
 }
